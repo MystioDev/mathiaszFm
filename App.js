b54c36ac37e43cd74c5760e4260638654f22b003
Mystio_ <104210683+MystioDev@users.noreply.github.com>
2023-02-03 22:35:54 +0100

    Deploy website

---
+++ App.js
@@ -0,0 +1,42 @@
+var swiper1 = new Swiper(".mySwiper1", {
+  effect: "cards",
+  direction: "vertical",
+  grabCursor: true,
+  autoplay: true,
+  speed: 1000,
+  loop: true,
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+  effect: "cards",
+  direction: "vertical",
+  grabCursor: true,
+  autoplay: true,
+  speed: 1000,
+  loop: true,
+});
+
+var swiper3 = new Swiper(".mySwiper3", {
+  effect: "cards",
+  direction: "vertical",
+  grabCursor: true,
+  autoplay: true,
+  speed: 1000,
+  loop: true,
+});
+
+window.addEventListener("load", () => {
+  document.getElementById("preloader").style.zIndex = "9999";
+  document.getElementById("preloader").style.height = "100%";
+  document.getElementById("preloader").style.width = "100%";
+  document.querySelector(".loader").style.display = "block";
+  document.querySelector("*").style.overflow = "hidden";
+
+  setTimeout(() => {
+    document.getElementById("preloader").style.zIndex = "-9999";
+    document.getElementById("preloader").style.height = "0";
+    document.getElementById("preloader").style.width = "0";
+    document.querySelector(".loader").style.display = "none";
+    document.querySelector("*").style.overflow = "auto";
+  }, 1300);
+});
