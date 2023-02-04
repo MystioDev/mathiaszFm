d587008f543834ca243347e378ecfac67a23ef1f
Mystio_ <104210683+MystioDev@users.noreply.github.com>
2023-02-04 19:59:43 +0100

    debug

--- App.js
+++ App.js
@@ -1,3 +1,19 @@
+window.addEventListener("load", () => {
+  document.getElementById("preloader").style.zIndex = "9999";
+  document.getElementById("preloader").style.height = "100%";
+  document.getElementById("preloader").style.width = "100%";
+  document.querySelector(".loader").style.display = "block";
+  document.querySelector("*").style.overflow = "hidden";
+
+  setTimeout(() => {
+    document.getElementById("preloader").style.zIndex = "-9999";
+    document.getElementById("preloader").style.height = "0";
+    document.getElementById("preloader").style.width = "0";
+    document.querySelector(".loader").style.display = "none";
+    document.querySelector("*").style.overflow = "auto";
+  }, 1300);
+});
+
 var swiper1 = new Swiper(".mySwiper1", {
   effect: "cards",
   direction: "vertical",
@@ -23,20 +39,4 @@ var swiper3 = new Swiper(".mySwiper3", {
   autoplay: true,
   speed: 1000,
   loop: true,
-});
-
-window.addEventListener("load", () => {
-  document.getElementById("preloader").style.zIndex = "9999";
-  document.getElementById("preloader").style.height = "100%";
-  document.getElementById("preloader").style.width = "100%";
-  document.querySelector(".loader").style.display = "block";
-  document.querySelector("*").style.overflow = "hidden";
-
-  setTimeout(() => {
-    document.getElementById("preloader").style.zIndex = "-9999";
-    document.getElementById("preloader").style.height = "0";
-    document.getElementById("preloader").style.width = "0";
-    document.querySelector(".loader").style.display = "none";
-    document.querySelector("*").style.overflow = "auto";
-  }, 1300);
 });
