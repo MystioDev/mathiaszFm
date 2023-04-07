bbcd8d47eb2bef76a75edb5910def6caa5e10677
Mystio_ <104210683+MystioDev@users.noreply.github.com>
2023-04-07 13:37:08 +0200

    Preloader bugfix

--- App.js
+++ App.js
@@ -1,19 +1,3 @@
-window.addEventListener("load", () => {
-  document.getElementById("preloader").style.zIndex = "9999";
-  document.getElementById("preloader").style.height = "100%";
-  document.getElementById("preloader").style.width = "100%";
-  document.querySelector(".loader").style.display = "block";
-  document.querySelector("*").style.overflow = "hidden";
-
-  setTimeout(() => {
-    document.getElementById("preloader").style.zIndex = "-9999";
-    document.getElementById("preloader").style.height = "0";
-    document.getElementById("preloader").style.width = "0";
-    document.querySelector(".loader").style.display = "none";
-    document.querySelector("*").style.overflow = "auto";
-  }, 1300);
-});
-
 var swiper1 = new Swiper(".mySwiper1", {
   effect: "cards",
   direction: "vertical",
